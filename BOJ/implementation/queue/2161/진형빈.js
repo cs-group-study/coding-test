6082901f770ada4c904ad7d0f41152e4e6123ad7
Theo Jin <jhb_august@naver.com>
2024-03-08 10:15:22 +0900

    240307/hyunbin 2161

---
+++ BOJ/implementation/queue/2161/진형빈.js
@@ -0,0 +1,20 @@
+//  문제 번호: https://www.acmicpc.net/problem/2161
+const readFileSync = require("fs").readFileSync;
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = readFileSync(filePath).toString().trim().split("\n");
+
+const count = Number(input[0]);
+
+const ogCard = [];
+Array.from({ length: count }, (_, i) => ogCard.push(i + 1));
+
+const ans = [];
+for (let i = 0; i <= count; i++) {
+	ans.push(ogCard[0]);
+    ogCard.shift()
+    ogCard.push(ogCard[0])
+    ogCard.shift()
+   
+}
+ans.pop()
+console.log(ans.join(" "))
